b51a9e45f0cb9f398d5143d2747c1faa9d32998a
nick-fournier <nichfournier@gmail.com>
2022-05-13 23:27:02 -0700

    basic file fetching and separated sparse class to file

--- lodes.py
+++ lodes.py
@@ -3,84 +3,88 @@ import pandas as pd
 import glob
 import os
 import shutil
-import sparse
 from tqdm.auto import tqdm
 from bs4 import BeautifulSoup
-from io import BytesIO
-from scipy.sparse import coo_matrix
-from pandas.api.types import CategoricalDtype
-
-
-class SparseLodes:
-    def df_to_sparse(self, df):
-        geocodes = pd.unique(df[['w_geocode', 'h_geocode']].values.ravel('K'))
-        jobs = [c for c in df.columns if c not in ['w_geocode', 'h_geocode']]
-        shape = (len(geocodes), len(geocodes))
-
-        # Map geocodes to coded index
-        self.jobs_cat = CategoricalDtype(categories=sorted(jobs), ordered=True)
-        self.geocodes_cat = CategoricalDtype(categories=sorted(geocodes), ordered=True)
-        self.h_geocode_index = df['h_geocode'].astype(self.geocodes_cat).cat.codes
-        self.w_geocode_index = df['w_geocode'].astype(self.geocodes_cat).cat.codes
-
-        # Create dict of sparse matrices
-        mat_dict = {k: coo_matrix((df[k], (self.w_geocode_index, self.h_geocode_index)), shape=shape) for k in jobs}
-
-        # Stack into an n*n*k sparse coordinatate (COO) matrix
-        # where n is the number of zones and k are the strata
-        self.sparse_mat = sparse.stack([sparse.COO(m) for m in mat_dict.values()], axis=-1)
 
 
 ###
-def get_cache():
-    cache = [f for f in glob.glob(os.path.join(os.getcwd(), '**/*.csv.gz'), recursive=True)]
+def get_cache_list(cache_dir=os.getcwd(), full_path=True):
+    if full_path:
+        return [f for f in glob.glob(os.path.join(cache_dir, '**/*.csv.gz'), recursive=True)]
+    else:
+        return [os.path.basename(f) for f in glob.glob(os.path.join(cache_dir, '**/*.csv.gz'), recursive=True)]
 
-def get_latest_year(hrefs):
-    years = [node.get('href').replace('.csv.gz', '')[-4:] for node in hrefs if node.get('href').endswith('.csv.gz')]
-    return max(years)
+def get_latest_year(base_url, state):
+    common_years = None
+    for zone in ['od', 'rac', 'wac']:
+        url = '/'.join([base_url, state.lower(), zone.lower()])
+        page = requests.get(url).text
+        hrefs = [x.get('href') for x in BeautifulSoup(page, 'html.parser').find_all('a')]
+        years = [node.replace('.csv.gz', '')[-4:] for node in hrefs if node.endswith('.csv.gz')]
 
-def file_fetch(url, filename):
-    print('Fetching ' + filename)
-    # make an HTTP request within a context manager
-    with requests.get('/'.join([url, filename]), stream=True) as r:
-        # check header to get content length, in bytes
-        total_length = int(r.headers.get("Content-Length"))
-        # implement progress bar via tqdm
-        with tqdm.wrapattr(r.raw, "read", total=total_length, desc="") as raw:
-            # save the output to a file
-            with open(os.path.join('cache', filename), 'wb') as output:
-                shutil.copyfileobj(raw, output)
+        if not common_years:
+            common_years = set(years)
+        else:
+            common_years = common_years.intersection(years)
 
-            df = pd.read_csv(BytesIO(r.content), compression="gzip", dtype={'h_geocode': str, 'w_geocode': str})
-            df.drop(columns='createdate', inplace=True)
+    return max(common_years)
 
-    return df
+def file_fetch(file_url, of_string="", fetch_only=True):
+    file_name = file_url.split('/')[-1]
+    file_path = os.path.join('cache', file_name)
 
-def lode_fetch(state, year):
-    state, year = 'ca', '2019'
-    zone = 'od'
-    filename = 'ca_od_main_JT00_2019.csv.gz'
+    # Check if already cached
+    if not os.path.exists(file_path):
+        desc_lab = ' '.join(['Fetching', file_name, of_string])
+        # make an HTTP request within a context manager
+        with requests.get(file_url, stream=True) as r:
+            # check header to get content length, in bytes
+            total_length = int(r.headers.get("Content-Length"))
+            # implement progress bar via tqdm
+            with tqdm.wrapattr(r.raw, "read", total=total_length, desc=desc_lab) as raw:
+                # save the output to a file
+                with open(file_path, 'wb') as output:
+                    shutil.copyfileobj(raw, output)
+
+    # Read the data into pandas
+    if not fetch_only:
+        print(' '.join(['Loading cached', file_name, of_string]))
+        df = pd.read_csv(file_path, compression="gzip", dtype={'h_geocode': str, 'w_geocode': str})
+        df.drop(columns='createdate', inplace=True)
+        return df
+    return
+
+def load_lodes(state, zone_types=None, year=None, fetch_only=False):
+    zone_types = ['od', 'rac', 'wac'] if not zone_types else zone_types
+    zone_types = [zone_types] if isinstance(zone_types, str) else zone_types
     base_url = 'https://lehd.ces.census.gov/data/lodes/LODES7'
 
     if not os.path.exists('cache'):
         os.mkdir('cache')
 
-    # if not year:
-    #     year = get_latest_year(hrefs)
-    #     print('No year specified, defaulting to latest year ' + year)
+    if not year:
+        print('No year specified', end=' ')
+        year = get_latest_year(base_url, state)
+        print(', defaulting to latest year ' + year)
 
-    for zone in ['od', 'rac', 'wac']:
+    for zone in zone_types:
         url = '/'.join([base_url, state.lower(), zone.lower()])
         page = requests.get(url).text
-        hrefs = BeautifulSoup(page, 'html.parser').find_all('a')
+        hrefs = [x.get('href') for x in BeautifulSoup(page, 'html.parser').find_all('a')]
+        flist = [node for node in hrefs if node.endswith('.csv.gz') and year in node]
 
-        flist = [node.get('href') for node in hrefs
-                 if node.get('href').endswith('.csv.gz') and year in node.get('href')]
+        # Downloading
+        lodes = {}
+        for fname in flist:
+            key = os.path.splitext(fname)[0]
+            of_string = '/'.join([str(flist.index(fname) + 1), str(len(flist))])
+            file_url = os.path.join(url, fname)
+            lodes[key] = file_fetch(file_url, of_string, fetch_only)
+        print('done')
 
-        tmp = file_fetch(url, filename)
-
-        tmp = {os.path.splitext(fname)[0]: file_fetch(url, fname) for fname in flist}
+    return lodes
 
 
 if __name__ == "__main__":
-    pass
+    tmp = load_lodes(state='ca', zone_types='wac', year='2019', fetch_only=True)
+
