f7159fd19345f545186303b5c716f40a6dd7c245
nick-fournier <nichfournier@gmail.com>
2022-05-20 08:27:30 -0700

    fetched block maps

--- geographies.py
+++ geographies.py
@@ -1,7 +1,6 @@
-import gzip
-import io
-import geopandas
-import zipfile
+import matplotlib.pyplot as plt
+import fiona
+import geopandas as gpd
 from utils import *
 from state_codes import State
 
@@ -16,30 +15,17 @@ def load_geoblocks(state, year='2021', cache=True):
     state_num = State.code[state.upper()]
 
     # Format the file url
-    url_template = 'https://www2.census.gov/geo/tiger/TGRGDB{yy}/tlgdb_{yyyy}_a_{state_num}_{state}.gdb.zip'
-    file_url = url_template.format(**{'yy': year[2:], 'yyyy': year, 'state_num': state_num, 'state': state})
+    url_template = 'https://www2.census.gov/geo/tiger/TIGER{year}/TABBLOCK/tl_{year}_{fips}_tabblock10.zip'
+    # url_template = 'https://www2.census.gov/geo/tiger/TGRGDB{yy}/tlgdb_{yyyy}_a_{state_num}_{state}.gdb.zip'
+    file_url = url_template.format(**{'year': year, 'fips': state_num})
 
     # Fetch the file from URL or cache
     bytes_data = fetch_bytes(file_url, cache=cache)
+    geodf = gpd.read_file('cache/geo/tiger/TIGER2019/TABBLOCK/tl_2019_25_tabblock10.zip')
 
-    # TODO need to read gdb to geopandas?
-    # Decompress the gzip bytes data
-    with zipfile.ZipFile(io.BytesIO(bytes_data)) as zbuf:
-        with zbuf.open(zbuf.namelist()[0]) as f:
-            geodata = f.read()
-            return f.read()
+    # # Read geodatabase into geopandas
+    # with fiona.io.ZipMemoryFile(bytes_data) as zip_memory_file:
+    #     with zip_memory_file.open(os.path.basename(file_url).rstrip('.zip')) as collection:
+    #         geodf = gpd.GeoDataFrame.from_features(collection)
 
-
-
-    # if ext == '.zip':
-    #     with ZipFile('ca_od_aux_JT00_2002.zip') as zf:
-    #         assert (len(zf.namelist()) == 1)
-    #         with zf.open(zf.namelist()[0]) as f:
-    #             return f.read()
-
-    # read into geopandas
-
-
-    print('done')
-
-    return lodes
+    return geodf
