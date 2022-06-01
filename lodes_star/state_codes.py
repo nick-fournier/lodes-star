6eace55a07782a0f5e99764d33b0cec630754d5b
nick-fournier <nichfournier@gmail.com>
2022-06-01 15:15:28 -0700

    added geographies

--- lodes_star/state_codes.py
+++ lodes_star/state_codes.py
@@ -6,6 +6,23 @@ class Geographies:
         csv_reader = csv.reader(csvfile)
         geographies = dict(csv_reader)
 
+    @classmethod
+    def list(cls):
+        whitespace = max([len(x) for x in cls.geographies.keys()])
+        for k, v in cls.geographies.items():
+            print(k, ' '*(whitespace - len(k)), v)
+        return
+
+
+    def __new__(cls, geography=None):
+        if geography in cls.geographies.keys():
+            return cls.geographies[geography]
+        else:
+            print("Geography not found")
+
+
+
+
 
 class State:
     with open('lodes_star/states.csv') as csvfile:
